39af0baf1cf01163f00266e670da0074fc948eeb
Okpainmo <okpainmoandrew@gmail.com>
2024-04-10 19:25:52 +0100

    refactor: updates to the markdown element config file

--- app/config/component-configurations.tsx
+++ app/config/component-configurations.tsx
@@ -1,38 +1,23 @@
 import SyntaxHighlighter from 'react-syntax-highlighter';
+import { HighlighterWrapper } from '../(routes)/posts/components/HighlighterWrapper';
 
 export const presetComponents = {
+  HighlighterWrapper,
   SyntaxHighlighter,
-  h1: (props: any) => (
-    <h1
-      className="font-extralight text-3xl sm:text-4xl md:text-5xl "
-      {...props}
-    />
-  ),
-  h2: (props: any) => (
-    <h2
-      className="font-extralight text-2xl sm:text-3xl md:text-4xl"
-      {...props}
-    />
-  ),
-  h3: (props: any) => (
-    <h3
-      className="font-extralight text-xl sm:text-2xl md:text-3xl"
-      {...props}
-    />
-  ),
-  h4: (props: any) => (
-    <h4
-      className="font-extralight text-[20px] sm:text-xl md:text-2xl"
-      {...props}
-    />
-  ),
+  h1: (props: any) => <h1 className="font-extralight text-2xl" {...props} />,
+  h2: (props: any) => <h2 className="font-extralight text-xl" {...props} />,
+  h3: (props: any) => <h3 className="font-extralight text-base" {...props} />,
+  a: (props: any) => <a className="text-gray-400 underline" {...props} />,
   li: (props: any) => <li className="list-inside list-disc mt-2" {...props} />,
+  em: (props: any) => (
+    <em className="relative top-1.5 text-[13px]" {...props} />
+  ),
   img: (props: any) => <img className="w-full" {...props} />,
   // img,
   p: (props: any) => <p className="my-4 leading-[25px]" {...props} />,
   blockquote: (props: any) => (
     <blockquote
-      className="py-4 px-8 italic bg-slate-900 text-white my-6"
+      className="py-4 px-8 italic bg-slate-700 border-l-4 border-gray-400 text-white my-6"
       {...props}
     />
   ),
