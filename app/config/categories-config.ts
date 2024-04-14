a3dd928daaf6c3034cff43275b6aef90a0eb964f
Okpainmo <okpainmoandrew@gmail.com>
2024-04-14 14:33:57 +0100

    refactor: updated categories config with real platform data

--- app/config/categories-config.ts
+++ app/config/categories-config.ts
@@ -1,36 +1,40 @@
 export const categoriesData = [
   {
     categoryName: 'Blog',
-    categoryBrief: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quod
-              animi quidem accusantium consequatur obcaecati repellat in sed.`,
+    categoryBrief: `A list of carefully and professionally written articles to keep you
+  current with trends and happening in the global crypto space, and the world of web3/blockchain engineering.`,
     categoryRoute: '/posts/blog',
     categoryId: 'blog'
   },
   {
     categoryName: 'Tutorials',
-    categoryBrief: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quod
-              animi quidem accusantium consequatur obcaecati repellat in sed.`,
+    categoryBrief: `A collection of raw, top-class web3/blockchain development tutorials, carefully and 
+  professionally written to help you master blockchain development. All tutorials are suitable for web3 'veterans' and
+  beginner alike.`,
     categoryRoute: '/posts/tutorials',
     categoryId: 'tutorials'
   },
   {
     categoryName: 'Learn Solidity',
-    categoryBrief: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quod
-              animi quidem accusantium consequatur obcaecati repellat in sed.`,
+    categoryBrief: `A collection of well ordered and professionally crafted articles/tutorials, teaching everything
+  you need, to build secure and efficient smart-contracts with the Solidity programming language. Suitable for
+  'veterans' and beginners alike. `,
     categoryRoute: '/posts/solidity',
     categoryId: 'solidity'
   },
   {
     categoryName: 'Learn Hardhat',
-    categoryBrief: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quod
-              animi quidem accusantium consequatur obcaecati repellat in sed.`,
+    categoryBrief: `Explore from a list of top-class Hardhat articles and tutorials carefully and professionally crafted to help
+  you master everything you need to know about web3 engineering with Hardhat. Suitable for
+  'veterans' and beginners alike.`,
     categoryRoute: '/posts/hardhat',
     categoryId: 'hardhat'
   },
   {
     categoryName: 'Learn Foundry',
-    categoryBrief: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quod
-              animi quidem accusantium consequatur obcaecati repellat in sed.`,
+    categoryBrief: `Explore from a list of top-class Foundry articles and tutorials carefully and professionally crafted to help
+  you master everything you need to know about web3 engineering with Foundry. Suitable for
+  'veterans' and beginners alike.`,
     categoryRoute: '/posts/foundry',
     categoryId: 'foundry'
   }
