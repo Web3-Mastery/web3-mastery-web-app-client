b00173273407488cedb897c3216ec09ee8a92195
Okpainmo <okpainmoandrew@gmail.com>
2024-04-15 16:46:39 +0100

    chore: added support for vercel insights and analytics to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from 'next';
 import './styles/globals.css';
 import { nunito_sans, poppins } from './utils/font';
 import { RTKProvider } from './rtk-base/provider';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://web3-mastery-web-app-client.vercel.app/')
@@ -17,7 +19,11 @@ export default function RootLayout({
   return (
     <RTKProvider>
       <html lang="en" className={`${nunito_sans} ${poppins}`}>
-        <body className="nunito_sans">{children}</body>
+        <body className="nunito_sans">
+          {children}
+          <Analytics />
+          <SpeedInsights />
+        </body>
       </html>
     </RTKProvider>
   );
