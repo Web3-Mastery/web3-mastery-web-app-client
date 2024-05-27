02ac672989ca2640cc1dcc2c708f014ed4ee1246
Okpainmo <okpainmoandrew@gmail.com>
2024-05-27 21:50:54 +0100

    refactor: categories config updates

--- app/config/categories-config.ts
+++ app/config/categories-config.ts
@@ -9,32 +9,39 @@ export const categoriesData = [
   {
     categoryName: 'Tutorials',
     categoryBrief: `A collection of raw, top-class web3/blockchain development tutorials, carefully and 
-  professionally written to help you master blockchain development. All tutorials are suitable for web3 'veterans' and
-  beginner alike.`,
+  professionally written to help you master blockchain development. All tutorials are suitable for all
+  categories of web3 enthusiasts - beginners, mid-level folks and 'veterans' alike.`,
     categoryRoute: '/posts/tutorials',
     categoryId: 'tutorials'
   },
   {
     categoryName: 'Learn Solidity',
     categoryBrief: `A collection of well ordered and professionally crafted articles/tutorials, teaching everything
-  you need, to build secure and efficient smart-contracts with the Solidity programming language. Suitable for
-  'veterans' and beginners alike. `,
+  you need, to build secure and efficient smart-contracts with the Solidity programming language. All content are suitable for beginners
+   mid-level folks and 'veterans' alike.`,
     categoryRoute: '/posts/solidity',
+    categoryId: 'solidity'
+  },
+  {
+    categoryName: 'The W3M Rust Bootcamp',
+    categoryBrief: `A list of well-crafted and top-class Rust tutorials, carefully and professionally written in line with 
+  a robust curriculum, to help you master everything you need, to start building projects(especially web3 projects and smart-contracts)
+  with the Rust programming language. The curriculum is suitable for all categories of web3 professionals.`,
+    categoryRoute: '/posts/rust',
     categoryId: 'solidity'
   },
   {
     categoryName: 'Learn Hardhat',
     categoryBrief: `Explore from a list of top-class Hardhat articles and tutorials carefully and professionally crafted to help
-  you master everything you need to know about web3 engineering with Hardhat. Suitable for
-  'veterans' and beginners alike.`,
+  you master everything you need to know about web3 engineering with Hardhat. Suitable for all categories of web3 professionals`,
     categoryRoute: '/posts/hardhat',
     categoryId: 'hardhat'
   },
   {
     categoryName: 'Learn Foundry',
     categoryBrief: `Explore from a list of top-class Foundry articles and tutorials carefully and professionally crafted to help
-  you master everything you need to know about web3 engineering with Foundry. Suitable for
-  'veterans' and beginners alike.`,
+  you master everything you need to know about web3 engineering with Foundry. Suitable for all categories of web3 professional - beginners
+   mid-level folks and 'veterans' alike.`,
     categoryRoute: '/posts/foundry',
     categoryId: 'foundry'
   }
