835aeb8564ac4591839870213fe3d1bf88f5b705
Okpainmo <okpainmoandrew@gmail.com>
2024-06-19 22:12:58 +0100

    chore: categories config updated to reflect new updates

--- app/config/categories-config.ts
+++ app/config/categories-config.ts
@@ -4,7 +4,9 @@ export const categoriesData = [
     categoryBrief: `A list of carefully and professionally written articles to keep you
   current with trends and happening in the global crypto space, and the world of web3/blockchain engineering.`,
     categoryRoute: '/posts/blog',
-    categoryId: 'blog'
+    categoryId: 'blog',
+    categoryType: 'platform-content',
+    categoryDomain: 'platform'
   },
   {
     categoryName: 'Tutorials',
@@ -12,7 +14,9 @@ export const categoriesData = [
   professionally written to help you master blockchain development. All tutorials are suitable for all
   categories of web3 enthusiasts - beginners, mid-level folks and 'veterans' alike.`,
     categoryRoute: '/posts/tutorials',
-    categoryId: 'tutorials'
+    categoryId: 'tutorials',
+    categoryType: 'platform-content',
+    categoryDomain: 'platform'
   },
   {
     categoryName: 'Learn Solidity',
@@ -20,7 +24,9 @@ export const categoriesData = [
   you need, to build secure and efficient smart-contracts with the Solidity programming language. All content are suitable for beginners
    mid-level folks and 'veterans' alike.`,
     categoryRoute: '/posts/solidity',
-    categoryId: 'solidity'
+    categoryId: 'solidity',
+    categoryType: 'learn-content',
+    categoryDomain: 'ethereum'
   },
   {
     categoryName: 'The W3M Rust Bootcamp',
@@ -28,14 +34,28 @@ export const categoriesData = [
   a robust curriculum, to help you master everything you need, to start building projects(especially web3 projects and smart-contracts)
   with the Rust programming language. The curriculum is suitable for all categories of web3 professionals.`,
     categoryRoute: '/posts/rust',
-    categoryId: 'solidity'
+    categoryId: 'solidity',
+    categoryType: 'learn-content',
+    categoryDomain: 'platform'
+  },
+  {
+    categoryName: 'Learn Solana Development',
+    categoryBrief: `Explore from a list of top-class Foundry articles and tutorials carefully and professionally crafted to help
+  you master everything you need to know about web3 engineering with Foundry. Suitable for all categories of web3 professional - beginners
+   mid-level folks and 'veterans' alike.`,
+    categoryRoute: '/posts/solana',
+    categoryId: 'foundry',
+    categoryType: 'learn-content',
+    categoryDomain: 'solana'
   },
   {
     categoryName: 'Learn Hardhat',
     categoryBrief: `Explore from a list of top-class Hardhat articles and tutorials carefully and professionally crafted to help
   you master everything you need to know about web3 engineering with Hardhat. Suitable for all categories of web3 professionals`,
     categoryRoute: '/posts/hardhat',
-    categoryId: 'hardhat'
+    categoryId: 'hardhat',
+    categoryType: 'learn-content',
+    categoryDomain: 'ethereum'
   },
   {
     categoryName: 'Learn Foundry',
@@ -43,6 +63,26 @@ export const categoriesData = [
   you master everything you need to know about web3 engineering with Foundry. Suitable for all categories of web3 professional - beginners
    mid-level folks and 'veterans' alike.`,
     categoryRoute: '/posts/foundry',
-    categoryId: 'foundry'
+    categoryId: 'foundry',
+    categoryType: 'learn-content',
+    categoryDomain: 'ethereum'
+  },
+  {
+    categoryName: 'Learn Solana Anchor',
+    categoryBrief: `A comprehensive tutorial series for learning smart contract development with Anchor - Solana's Sealevel 
+    run-time framework. Dive in to explore top quality content to help level up your smart contract development on the Solana network`,
+    categoryRoute: '/posts/anchor',
+    categoryId: 'foundry',
+    categoryType: 'learn-content',
+    categoryDomain: 'solana'
+  },
+  {
+    categoryName: 'The Web3 Mastery WSL Handbook',
+    categoryBrief: `A comprehensive and simplified tutorial series for configuring and working with WSL(Windows Sub-system for Linux)
+    on the Windows operating system.`,
+    categoryRoute: '/posts/wsl',
+    categoryId: 'wsl',
+    categoryType: 'learn-content',
+    categoryDomain: 'platform'
   }
 ];
