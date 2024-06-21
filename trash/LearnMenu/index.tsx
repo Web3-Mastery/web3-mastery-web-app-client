481575b4c0f1e7da22271c58d4ec0531e8d542b3
Okpainmo <okpainmoandrew@gmail.com>
2024-06-21 09:13:42 +0100

    chore: trashed content

---
+++ trash/LearnMenu/index.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import Link from 'next/link';
+
+// function LearnMenu() {
+//   return (
+//     <div
+//       className={`learn-menu ${showLearnMenu ? 'inline-block' : 'hidden'} hidden card-glass_dark w-[300px] absolute z-20 right-[0] sm:right-[50px] lg:right-[-50px] top-[295px] md:top-[285px] lg:top-[30px] border border-gray-200 rounded-[5px] px-4 py-6`}
+//     >
+//       <ul className=" uppercase font-normal flex flex-col text-center sm:mx-auto">
+//         <div className="link-group">
+//           <Link
+//             href="/posts/solidity"
+//             onClick={() => {
+//               dispatch(slideNavOut());
+//               toggleLearnMenu();
+//             }}
+//           >
+//             <li className="border-b border-gray-300 py-5">Learn Solidity</li>
+//           </Link>
+//           <Link
+//             href="/posts/hardhat"
+//             onClick={() => {
+//               dispatch(slideNavOut());
+//               toggleLearnMenu();
+//             }}
+//           >
+//             <li className="border-b border-gray-300 py-5">Learn Hardhat</li>
+//           </Link>
+//           <Link
+//             href="/posts/foundry"
+//             onClick={() => {
+//               dispatch(slideNavOut());
+//               toggleLearnMenu();
+//             }}
+//           >
+//             <li className="border-b border-gray-300 py-5">Learn Foundry</li>
+//           </Link>
+//         </div>
+//         <div className="link-group">
+//           <Link
+//             href="/posts/rust"
+//             onClick={() => {
+//               dispatch(slideNavOut());
+//               toggleLearnMenu();
+//             }}
+//           >
+//             <li className="border-b border-gray-300 py-5">
+//               The W3M Rust Bootcamp
+//             </li>
+//           </Link>
+//           <Link
+//             href="/posts/solana"
+//             onClick={() => {
+//               dispatch(slideNavOut());
+//               toggleLearnMenu();
+//             }}
+//           >
+//             <li className="border-b border-gray-300 py-5">
+//               Learn Solana Development
+//             </li>
+//           </Link>
+//           <Link
+//             href="/posts/anchor"
+//             onClick={() => {
+//               dispatch(slideNavOut());
+//               toggleLearnMenu();
+//             }}
+//           >
+//             <li className="border-b border-gray-300 py-5">
+//               Learn Solana Anchor
+//             </li>
+//           </Link>
+//         </div>
+//         {/* <button className="bg-slate-950 px-4 py-4 uppercase text-normal text-white mt-6">
+//             Connect With Wallet
+//           </button> */}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default LearnMenu;
